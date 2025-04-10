529ed41593ab28fd9487013a7aa58de9785c9b2a
tkdenddl1324 <tkdenddl1324@gmail.com>
2025-04-10 15:46:35 +0900

    first-commit

---
+++ src/test/java/kr/co/tkdenddl1324/SpringJenkinsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.tkdenddl1324;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
